8215cfcc2f6fcaa78fc3c03b70b162ba348b37a3
Ashley Herrera <ashleyherrera131@gmail.com>
2021-02-09 18:00:06 -0700

    more practice

--- script.js
+++ script.js
@@ -20,8 +20,21 @@ let people = [{
 }]
 
 function greet(data){
-    for(let i =0; i <data.length; i++){
-        let person = data[i]
+    // for(let i =0; i <data.length; i++){
+    //     let person = data[i]
+    //     if(person.role == "Instruct"){
+    //         console.log(`Welcome ${person.name} ${person.role}`)
+    //     }
+    //     else if(person.role == "Admin"){
+    //         console.log(`Welcome ${person.name} ${person.role}, Have a great day!`)
+    //     }
+    //     else{
+    //         console.log(`${person.name} is not a Codeworks employee.`)
+    //     }
+    // }
+
+    // Replaces line 23 and 24
+    data.forEach(person =>{
         if(person.role == "Instruct"){
             console.log(`Welcome ${person.name} ${person.role}`)
         }
@@ -31,7 +44,7 @@ function greet(data){
         else{
             console.log(`${person.name} is not a Codeworks employee.`)
         }
-    }
+    })
 }
-
+// This is still required
 greet(people)
